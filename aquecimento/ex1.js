7d565eae6bd2a5d05794a88dc10144d542d23c63
Filipe Freitas <2184937+filipejsfreitas@users.noreply.github.com>
2021-01-20 16:00:53 +0000

    Adicionar o aquecimento e o PR atualizado

---
+++ aquecimento/ex1.js
@@ -0,0 +1,21 @@
+const axios = require('axios').default;
+const token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwMDgxMGM2NDFhYmQ1NDU0MDZkZmRkMSIsImxldmVsIjoyLCJlbnRpZGFkZSI6ImVudF9BM0VTIiwiZW1haWwiOiJkYXcyMDIwQHRlc3RlLnVtaW5oby5wdCIsImlhdCI6MTYxMTE1MzcyMSwiZXhwIjoxNjExMTgyNTIxfQ.JdgVjkQKAhnxxDrjgwSswN9Ay7MkngglGJBJw66DsMxs-Xu5oOHuXqHOGa4OzqXOlM8KXgrwhIoXPu_OWQdBS48ouSuZLP-ubzVFZgkv6o0v-rTF8VlkHCdId4tM9KSr8ps8fdgCpYGB9rTpImmsAPJ5HTV-7iWxxjilBjsDKM7d7llXn8S6Pp89XwdP-N6qCQ59fC9wfzMt6rhITq4ifLGlatxNsMtEzhI0WJi_uc0LIgLue-VhcMwtKZxNZfQJi3ptkNFa-o77R7Y5M9cElCP5Tt8v9KewiSzCAhYRJvCpA05tnGMPYY3S1JqRosuR0s8Db5i73XkQbXV40hiM0g';
+const baseUrl = 'http://clav-api.di.uminho.pt/v2/classes';
+
+(async function() {
+    const processosBase = await axios.get(baseUrl + '/c900?token=' + token);
+
+    const processosNivel3 = [];
+
+    for(let filho of processosBase.data.filhos) {
+        const processosNivel2 = await axios.get(baseUrl + '/c' + filho.codigo + '?token=' + token);
+
+        for(let filho of processosNivel2.data.filhos) {
+            processosNivel3.push(filho.codigo);
+        }
+    }
+
+    return [ processosNivel3, processosNivel3.length ];
+})()
+.then(data => console.log(JSON.stringify(data)))
+.catch(err => console.error(err));
---
+++ aquecimento/ex4.js
@@ -0,0 +1,11 @@
+const axios = require('axios').default;
+const token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwMDgxMGM2NDFhYmQ1NDU0MDZkZmRkMSIsImxldmVsIjoyLCJlbnRpZGFkZSI6ImVudF9BM0VTIiwiZW1haWwiOiJkYXcyMDIwQHRlc3RlLnVtaW5oby5wdCIsImlhdCI6MTYxMTE1MzcyMSwiZXhwIjoxNjExMTgyNTIxfQ.JdgVjkQKAhnxxDrjgwSswN9Ay7MkngglGJBJw66DsMxs-Xu5oOHuXqHOGa4OzqXOlM8KXgrwhIoXPu_OWQdBS48ouSuZLP-ubzVFZgkv6o0v-rTF8VlkHCdId4tM9KSr8ps8fdgCpYGB9rTpImmsAPJ5HTV-7iWxxjilBjsDKM7d7llXn8S6Pp89XwdP-N6qCQ59fC9wfzMt6rhITq4ifLGlatxNsMtEzhI0WJi_uc0LIgLue-VhcMwtKZxNZfQJi3ptkNFa-o77R7Y5M9cElCP5Tt8v9KewiSzCAhYRJvCpA05tnGMPYY3S1JqRosuR0s8Db5i73XkQbXV40hiM0g';
+const baseUrl = 'http://clav-api.di.uminho.pt/v2/classes';
+
+(async function() {
+    const processosBase = await axios.get(baseUrl + '/c900.10.505/procRel/?token=' + token);
+
+    return processosBase.data.length;
+})()
+.then(data => console.log(JSON.stringify(data)))
+.catch(err => console.error(err));
